f52d927752d99ac61e068e88f90287706ea45034
Sandbagger <wneal_19@yahoo.co.uk>
2019-03-21 06:52:33 +0000

    Added useSate

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
+
+  const [quote, setquote] = useState("This is a quote")
+  
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {quote}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
-}
+
 
 export default App;
